05e49cccd817bcfc226396c849bdbc134f395b50
ShakhawathHossain <shakhawathnur@gmail.com>
2019-09-22 12:47:57 +0600

    30 Days of code solution

---
+++ Day 1.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+I=int(input())
+D=float(input())
+string=input()
+
+intsum=i+I
+floatsum=d+D
+stringsum=s+string
+
+print(intsum)
+print(floatsum)
+print(stringsum)
