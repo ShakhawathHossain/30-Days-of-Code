405454d7bffa5a43bb7490b5fece567049fbe668
ShakhawathHossain <shakhawathnur@gmail.com>
2019-10-02 23:21:41 +0600

    30 Days of code solution

---
+++ Day 12.py
@@ -0,0 +1,44 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber,score):
+        self.score=score
+        super(Student,self).__init__(firstName, lastName, idNumber)
+    def calculate(self):
+        total=0
+
+        for i in scores:
+            total+=i
+        avg= total/len(scores)
+
+        if 90<=avg<=100:
+            return 'O'
+        elif 80<=avg<=90:
+            return 'E'
+        elif 70<=avg<=80:
+            return 'A'
+        elif 55<=avg<=70:
+            return 'P'
+        elif 40<=avg<=55:
+            return 'D'
+        elif avg<40:
+            return 'T'
+
+
+
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
